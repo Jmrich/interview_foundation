1f43b24e72eab6f8a60c8638944089d6650ae6e5
John Richardson <johnrichardson@Jrichs-MacBook-Pro.local>
2020-09-10 15:50:52 -0700

    add ui packages and setup auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
@@ -17,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('github_token')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            @guest
+                <div class="card">
+                    <div class="card-body">
+                        <a href="{{ route('demo-login') }}" class="btn btn-primary ml-auto mr-auto" style="width: 50%;">Login</a>
+                    </div>
+                </div>
+            @else
+
+            @endguest
+        </div>
+    </div>
+</div>
+@endsection
